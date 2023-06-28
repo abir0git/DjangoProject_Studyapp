fa31301ad6a2a3d5da24b895fb49130a8e5af635
Abir <royabirlebachun0@gmail.com>
2023-06-28 12:54:24 +0530

    Modal done

--- project/static/js/script.js
+++ project/static/js/script.js
@@ -88,3 +88,27 @@ eye.addEventListener("click", function(){
   const type = passwordInput.getAttribute("type") === "password" ? "text" : "password"
   passwordInput.setAttribute("type", type)
 })
+
+// Modal for error messages
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  console.log('jk');
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
